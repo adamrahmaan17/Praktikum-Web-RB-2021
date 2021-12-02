53b602201219affc8321151a9ac7fc808655a38e
adamrahmaan17 <74776786+adamrahmaan17@users.noreply.github.com>
2021-12-02 14:24:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<form method="GET" action="hasil_hitung.php">
+Nilai y : <input type="text" name="a"><br><br>
+Nilai x : <input type="text" name="b"><br><br>
+Operasi : <br>
+<input type="radio" name="operasi" value="jumlah">Penjumlahan<br>
+<input type="radio" name="operasi" value="kurang">Pengurangan<br> 
+<input type="radio" name="operasi" value="kali">Perkalian<br> 
+<input type="radio" name="operasi" value="bagi">Pembagian<br><br> 
+<input type="submit" value="Hitung">
+</form>
